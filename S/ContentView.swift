1686ad35bcc7bf606f446278349c36f9c1e0ab9d
Pravin Kamble <pprravinn@gmail.com>
2025-01-30 16:45:11 +0530

    Initial Commit

---
+++ S/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  S
+//
+//  Created by Pravin Kamble on 30/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ S/SApp.swift
@@ -0,0 +1,17 @@
+//
+//  SApp.swift
+//  S
+//
+//  Created by Pravin Kamble on 30/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
